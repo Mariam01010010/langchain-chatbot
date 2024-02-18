da3e8454e0b75bb1acab428338aeb2662c18ec14
Mariam01010010 <mariamraashid@hotmail.com>
2024-02-18 15:11:21 +0000

    LLM Chain section of documentation complete

--- basics.js
+++ basics.js
@@ -1,3 +1,4 @@
+// LLM Chain: Part 1
 import { config } from "dotenv";
 config();
 
@@ -7,4 +8,38 @@ const chatModel = new ChatOpenAI({});
 
 const response = await chatModel.invoke("what is LangSmith?");
 
-console.log(response);
+// console.log("response", response);
+console.log("content", response["content"]);
+
+// import { Configuration, OpenAIApi } from "openai";
+// import { ChatOpenAI } from "@langchain/openai";
+
+// LLM Chain: Part 2
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a world class technical documentation writer."],
+  ["user", "{input}"],
+]);
+
+const chain = prompt.pipe(chatModel);
+const promptedResponse = await chain.invoke({
+  input: "what is LangSmith?",
+});
+
+// console.log({ prompt, chain });
+// console.log("promptedResponse", promptedResponse);
+console.log("prompted content", promptedResponse.content);
+
+// LLM Chain: Part 3
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const outputParser = new StringOutputParser();
+
+const llmChain = prompt.pipe(chatModel).pipe(outputParser);
+
+const stringResponse = await llmChain.invoke({
+  input: "what is LangSmith?",
+});
+
+console.log("stringResponse", stringResponse);
